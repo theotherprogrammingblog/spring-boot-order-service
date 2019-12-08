7f8c5982ddfee0e549627410e679202d8c290e1f
theotherprogrammingblog <theotherprogrammingblog@gmail.com>
2019-12-08 03:07:47 -0600

    updating config

---
+++ src/test/java/com/theotherprogrammingblog/springbootorderservice/SpringBootOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theotherprogrammingblog.springbootorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
